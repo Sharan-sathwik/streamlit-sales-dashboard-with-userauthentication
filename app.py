ad856833b506148876401f2347466c20c12f9217
Sharan-sathwik <108509483+Sharan-sathwik@users.noreply.github.com>
2022-07-05 09:43:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
-import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+import streamlit_authenticator as stauth  # pip install streamlit-authenticator-0.1.5
 
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
